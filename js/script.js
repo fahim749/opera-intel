619a20e7a68c069af1e8e4dabd0b6436e02342c5
fahim749 <fahim947@gmail.com>
2025-06-25 11:28:14 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('Site loaded!');
   // Add animations or dynamic content here
 });
+
+/*=============PM dashboard=================*/
+
+
+function simulateKPI() {
+  document.querySelectorAll('.kpi-bar').forEach(bar => {
+    const currentWidth = parseInt(bar.style.width);
+    bar.style.width = `${currentWidth + 10}%`;
+  });
+}
